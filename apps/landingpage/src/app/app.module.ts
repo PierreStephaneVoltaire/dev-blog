8f7f629f650ef81d7e4076b6e0a3e607928d17f7
steph <psvoltaire96@gmail.com>
2020-03-08 06:21:11 -0400

    first commit

--- apps/landingpage/src/app/app.module.ts
+++ apps/landingpage/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CardComponent } from './components/common/card/card.component';
+import { PostsComponent } from './components/posts/posts.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CardComponent, PostsComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
